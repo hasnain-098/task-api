bb2787f4622ac767e111dc1c969083672d0da702
Hasnain Memon <hasnainmemon.018@gmail.com>
2026-07-23 17:14:29 +0500

    Stage 1: connect via .env and create table

--- database.py
+++ database.py
@@ -1,35 +1,43 @@
-import sqlite3
+import os
+import psycopg
+from dotenv import load_dotenv
 
-conn = sqlite3.connect(
-    "tasks.db",
-    check_same_thread=False
+load_dotenv()
+
+conn = psycopg.connect(
+    host=os.getenv("DB_HOST"),
+    port=(os.getenv("DB_PORT")),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
 )
 
-conn.row_factory = sqlite3.Row
-cursor = conn.cursor()
+conn.autocommit = True
+cursor = conn.cursor(row_factory=psycopg.rows.dict_row)
 
 def initialize_database():
-
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS tasks(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        done BOOLEAN NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-    """)
-
-    conn.commit()
+       CREATE TABLE IF NOT EXISTS tasks(
+           id SERIAL PRIMARY KEY,
+           title VARCHAR(255) NOT NULL,
+           done BOOLEAN NOT NULL DEFAULT FALSE,
+           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+           updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+           )
+       """)
 
     cursor.execute("SELECT COUNT(*) FROM tasks")
+    count = cursor.fetchone()["count"]
 
-    if cursor.fetchone()[0] == 0:
+    if count > 0:
+        return
+
+    if count == 0:
 
         cursor.executemany(
             """
             INSERT INTO tasks(title,done)
-            VALUES(?,?)
+            VALUES(%s,%s)
             """,
             [
                 ("Learn FastAPI", False),
@@ -37,7 +45,5 @@ def initialize_database():
                 ("Push to GitHub", True)
             ]
         )
-
-        conn.commit()
 
 initialize_database()
